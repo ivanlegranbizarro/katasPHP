32780e9ada40b507efb56728c2e4a559ea9fd28a
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-09-28 11:24:57 +0200

    Exception to Jordi

--- numeros_vivos.php
+++ numeros_vivos.php
@@ -9,21 +9,17 @@ Repite estas reglas hasta que el número llegue a 1.
 
 El resultado final será la cantidad de veces que se aplicaron las reglas antes de alcanzar el 1. */
 
-function juego_numeros_vivos(int $num): int
+function juego_numeros_vivos(int $num): int|string
 {
     $resultado = 0;
     while ($num != 1 && $num != 0) {
         $num = match (true) {
             str_contains((string) $num, '5') => (int) str_replace('5', '0', (string) $num),
             $num % 2 == 0 => $num / 2,
+            $num == 3 => throw new Exception('Jordi, vaya tramposo estás hecho'),
             default => $num + 3,
         };
         $resultado++;
     }
     return $resultado;
 }
-
-echo "Resultado para 5: " . juego_numeros_vivos(5) . "<br>";
-echo "Resultado para 15: " . juego_numeros_vivos(15) . "<br>";
-echo "Resultado para 0: " . juego_numeros_vivos(0) . "<br>";
-echo "Resultado para 16: " . juego_numeros_vivos(16) . "<br>";
